daeb55c14188ed9caf22832003d92a2707735836
Reza Hidayat <m.rezahidayat.rh@gmail.com>
2023-11-13 09:20:59 +0700

    update profile

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,76 @@
-# Cek apakah Git terinstal, jika tidak, install Git
+# cek winget sudah terinstall atau belum
+if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
+    Write-Host "Error: winget is not installed. Please install winget and run the script again."
+    exit
+}
+
+# cek git sudah terinstall atau belum
 if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
     Write-Host "Git is not installed. Installing Git..."
     winget install -e --id Git.Git
+}
+
+# cek oh-my-posh sudah terinstall atau belum
+$ohMyPoshInstalled = Get-Command oh-my-posh -ErrorAction SilentlyContinue
+
+if (-not $ohMyPoshInstalled) {
+    Write-Host "Oh My Posh is not installed. Installing Oh My Posh..."
+    winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh
+}
+else {
+    Write-Host "Oh My Posh is already installed."
+}
+
+# penginstalan Font
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+$fontFamilies = (New-Object System.Drawing.Text.InstalledFontCollection).Families
+if ($fontFamilies -notcontains "CaskaydiaCove NF") {
+    # Download Font Dari github
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile("https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip", ".\CascadiaCode.zip")
+
+    # extract font dan install ke direktori font windows
+    Expand-Archive -Path ".\CascadiaCode.zip" -DestinationPath ".\CascadiaCode" -Force
+    $destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
+    Get-ChildItem -Path ".\CascadiaCode" -Recurse -Filter "*.ttf" | ForEach-Object {
+        If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {        
+            # Install font
+            $destination.CopyHere($_.FullName, 0x10)
+        }
+    }
+    # Bersihkan file yang tidak diperlukan setelah instalasi font
+    Remove-Item -Path ".\CascadiaCode" -Recurse -Force
+    Remove-Item -Path ".\CascadiaCode.zip" -Force
+}
+
+# cek chocolatey sudah terinstall atau belum
+$chocolateyInstalled = Get-Command choco -ErrorAction SilentlyContinue
+if (-not $chocolateyInstalled) {
+    # Set execution policy and download/run Chocolatey installer
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+else {
+    Write-Host "Chocolatey is already installed."
+}
+
+# cek fzf sudah terinstall atau belum
+$fzfInstalled = Get-Command fzf -ErrorAction SilentlyContinue
+if (-not $fzfInstalled) {
+    choco install fzf -y
+}
+else {
+    Write-Host "fzf is already installed."
+}
+
+# cek gsudo sudah terinstall atau belum
+$gsudoInstalled = Get-Command gsudo -ErrorAction SilentlyContinue
+if (-not $gsudoInstalled) {
+    choco install gsudo -y
+}
+else {
+    Write-Host "gsudo is already installed."
 }
 
 # Membuat direktori jika tidak ada
@@ -10,8 +79,18 @@ if (!(Test-Path -Path $githubDirectory -PathType Container)) {
     New-Item -Path $githubDirectory -ItemType Directory | Out-Null
 }
 
-# Navigasi kembali ke direktori sebelumnya
-Set-Location -Path $PWD.Path
+
+# Fungsi untuk membuka PowerShell sebagai administrator
+function Run-As-Administrator {
+    param([scriptblock]$ScriptBlock)
+    Start-Process -FilePath powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command $($ScriptBlock | Out-String)" -Verb RunAs
+}
+
+# Skrip untuk dijalankan sebagai administrator
+$adminScript = {
+# clone github repository
+Set-Location -Path $env:userprofile\Documents\Github
+git clone "https://github.com/rezapace/ps"
 
 # Periksa apakah file profil pengguna ada, jika tidak, buat profil baru
 if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
@@ -45,63 +124,18 @@ else {
 # Eksekusi profil
 & $profile
 
-# Instalasi Oh My Posh menggunakan Windows Package Manager (winget)
-winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh
-
-
-# Instalasi font jika tidak ada
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
-$fontFamilies = (New-Object System.Drawing.Text.InstalledFontCollection).Families
-if ($fontFamilies -notcontains "CaskaydiaCove NF") {
-    # Unduh font dari GitHub
-    $webClient = New-Object System.Net.WebClient
-    $webClient.DownloadFile("https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip", ".\CascadiaCode.zip")
-
-    # Ekstrak font dan instalasi ke direktori Fonts Windows
-    Expand-Archive -Path ".\CascadiaCode.zip" -DestinationPath ".\CascadiaCode" -Force
-    $destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
-    Get-ChildItem -Path ".\CascadiaCode" -Recurse -Filter "*.ttf" | ForEach-Object {
-        If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {        
-            # Install font
-            $destination.CopyHere($_.FullName, 0x10)
-        }
-    }
-    # Bersihkan file yang tidak diperlukan setelah instalasi font
-    Remove-Item -Path ".\CascadiaCode" -Recurse -Force
-    Remove-Item -Path ".\CascadiaCode.zip" -Force
-}
-
-# Set execution policy dan unduh serta jalankan Chocolatey installer
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
 # Instalasi modul-modul yang dibutuhkan melalui PowerShellGet dan Chocolatey
 Install-Module -Name Terminal-Icons -Repository PSGallery -Force
 Install-Module -Name posh-git -Scope CurrentUser -Force
 Install-Module -Name PowerShellGet -Scope CurrentUser -Force
 Install-Module -Name z -Scope CurrentUser -Force
 Install-Module -Name PSReadLine -Scope CurrentUser -Force
-choco install fzf -y
 Install-Module -Name PSFzf -Scope CurrentUser -Force
-choco install gsudo -y
 
 # Set execution policy
 Set-ExecutionPolicy RemoteSigned
 Set-ExecutionPolicy Restricted
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-
-# Fungsi untuk membuka PowerShell sebagai administrator
-function Run-As-Administrator {
-    param([scriptblock]$ScriptBlock)
-    Start-Process -FilePath powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command $($ScriptBlock | Out-String)" -Verb RunAs
-}
-
-# Skrip untuk dijalankan sebagai administrator
-$adminScript = {
-    # Navigasi ke direktori Github
-    Set-Location -Path $env:userprofile\Documents\Github
-
-    # Clone repositori dari GitHub
-    git clone "https://github.com/rezapace/ps"
 }
 
 # Jalankan skrip sebagai administrator
